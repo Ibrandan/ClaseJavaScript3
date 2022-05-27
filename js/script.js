7cfe9587dad5a6881494693dba370d6e6643b567
Ignacio Brandán <brandanignaciod@gmail.com>
2022-05-27 00:34:08 -0300

    Orden de Js

--- js/script.js
+++ js/script.js
@@ -1,53 +1,16 @@
-// function suma(n1,n2){
-//     return n1 + n2;
-// }
+function preguntarNumero() {
+    let primerNum = prompt('Ingrese el primer numero');
+    let segundoNum = prompt('Ingrese el segundo numero');
 
-// function resta(n1,n2){
-//     return n1 - n2;
-// }
+    return {
+        "n1":parseFloat(primerNum), "n2":parseFloat(segundoNum)
+    }
+}
 
-// function multiplicacion(n1,n2){
-//     return n1 * n2;
-// }
-
-// function division(n1,n2){
-//     return n1 / n2;
-// }
-
-// function cuadradoDe(num){
-//     return multiplicacion(num, num)
-// }
-
-// function potencia(num, n) {
-//     let resultado = 1;
-//     for(let i = 0; i < n; i++) {
-//         resultado += num;
-//     }
-//     return resultado;
-// }
-
-// function potenciaRec(num, n) {
-//     if(n == 0) {
-//         return 1;
-//     }
-//     if(n == 1) {
-//         return num;
-//     }
-//     return num*potenciaRec(num, n-1);
-// }
-
-// let rectangulo = {
-//     'color' :   'rojo',
-//     'alto'  :   '25',
-//     'ancho' :   '15',
-//     'relleno'   :   'rojo'
-// }
-// // clave: valor
-// // atributo: valor
-// let ingresadoPorElUsuario = 'color'
-
-// let variable = null;
-// let variable2 = undefined;
+function mostrarResultado(resultado){
+    let nodo = document.getElementById("resultado");
+    nodo.innerText = resultado;
+}
 
 function sumar(n1,n2){
     let resultado =0;
@@ -79,18 +42,4 @@ function dividir(n1,n2){
 
     resultado = numeros.n1 / numeros.n2;
     mostrarResultado(resultado);
-}
-
-function preguntarNumero() {
-    let primerNum = prompt('Ingrese el primer numero');
-    let segundoNum = prompt('Ingrese el segundo numero');
-
-    return {
-        "n1":parseFloat(primerNum), "n2":parseFloat(segundoNum)
-    }
-}
-
-function mostrarResultado(resultado){
-    let nodo = document.getElementById("resultado");
-    nodo.innerText = resultado;
 }
